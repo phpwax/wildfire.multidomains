788fbdb9d91ad66faa2289f49687452c4d2060bc
charlesmarshall <cm56marshall@googlemail.com>
2012-07-09 18:24:52 +0100

    trim off the / at either ends and add them in again

--- lib/model/DomainContent.php
+++ lib/model/DomainContent.php
@@ -2,9 +2,11 @@
 class DomainContent extends WildfireContent{
   public $table = "wildfire_content";
 
+
+
   public function permalink($obj=false){
     if(!$obj) return $this->permalink;
-    else return str_replace($obj->permalink, "", $this->permalink);
+    else return "/".ltrim(rtrim(str_replace($obj->permalink, "", $this->permalink), "/")."/", "/");
   }
 }
 ?>
